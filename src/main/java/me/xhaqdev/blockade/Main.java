3ab014b67acfdc7dbe01222c366965eed5b3bbef
xHaqDEV <30455901+xHaqDEV@users.noreply.github.com>
2017-07-26 06:26:56 +0200

    Add files via upload

---
+++ src/main/java/me/xhaqdev/blockade/Main.java
@@ -0,0 +1,12 @@
+package me.xhaqdev.blockade;
+
+import me.xhaqdev.blockade.listeners.BlocadeListeners;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new BlocadeListeners(), this);
+    }
+}
---
+++ src/main/java/me/xhaqdev/blockade/listeners/BlocadeListeners.java
@@ -0,0 +1,18 @@
+package me.xhaqdev.blockade.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFromToEvent;
+
+public class BlocadeListeners implements Listener {
+
+    @EventHandler
+    public void onBlockade(BlockFromToEvent e) {
+        Material m = e.getBlock().getType();
+        if (m == Material.WATER || m == Material.STATIONARY_WATER || m == Material.LAVA || m == Material.STATIONARY_LAVA) {
+            e.setCancelled(true);
+        }
+    }
+}
