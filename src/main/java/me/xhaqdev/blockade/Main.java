f10f43bef6168903f3331330d72af13307a2a004
xHaqDEV <30455901+xHaqDEV@users.noreply.github.com>
2017-07-26 15:58:01 +0200

    Update Main.java

--- src/main/java/me/xhaqdev/blockade/Main.java
+++ src/main/java/me/xhaqdev/blockade/Main.java
@@ -5,7 +5,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+    
+    @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new BlocadeListeners(), this);
     }
